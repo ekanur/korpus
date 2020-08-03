6a59b25d91a6a5968cfd48452969b9448597f724
ekanur <eka.html@gmail.com>
2020-08-03 21:08:14 +0700

    styling

--- resources/views/template/adminsidebar.blade.php
+++ resources/views/template/adminsidebar.blade.php
@@ -52,3 +52,13 @@
         </div>
     </div>
 </nav>
+
+@push('style')
+<style type="text/css">
+    .navbar-vertical.navbar-expand-xs .navbar-nav>.nav-item>.nav-link.active {
+        background: #8e8e8e;
+        color: white;
+        font-weight: bold !important;
+    }
+</style>
+@endpush
--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" href="{{ asset("assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/vendor/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/vendor/datatables.net-select-bs4/css/select.bootstrap4.min.css") }}">
+    @stack('style')
 </head>
 
 <body>
