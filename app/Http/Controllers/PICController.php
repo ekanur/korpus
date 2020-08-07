915ea1e4a73f3f7332713acfc028ae75c7603cb4
ekanur <eka.html@gmail.com>
2020-08-07 13:43:41 +0700

    simpan sub-kategori

--- app/Http/Controllers/PICController.php
+++ app/Http/Controllers/PICController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Kategori;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -29,20 +30,32 @@ class PICController extends Controller
 
     public function simpanKategori(Request $request)
     {
-        // $array_sub = explode(",", $request->sub_kategori);
-        // dd($array_sub);
         $kategori = new Kategori;
         $kategori->korpus_id = Auth::user()->korpus->id;
         $kategori->kategori = $request->kategori;
         $kategori->parent_id = 0;
         $kategori->save();
 
-        // if($request->sub_kategori != null){
-        //     $sub_kategori = Kategori::find($kategori->id);
-        //     $sub_kategori->korpus_id = Auth::user()->korpus->id;
-        // }
+        if($request->sub_kategori != null){
+            $korpus_id = Auth::user()->korpus->id;
+            $parent_id = $kategori->id;
+            $array_sub = explode(",", $request->sub_kategori);
+            $sub_kategori = array_map(fn($sub) => trim($sub), $array_sub);
+            $sub_kategori = array_map(function($sub) use($korpus_id, $parent_id){
+
+                return array("korpus_id" => $korpus_id, "kategori" => $sub, "parent_id" => $parent_id, "created_at" => Carbon::now()->toDateTimeString());
+            }, $sub_kategori);
+            $simpan_sub_kategori = Kategori::insert($sub_kategori);
+        }
 
         return redirect()->back()->with("msg_success", "Berhasil membuat kategori baru.");
+    }
+
+    public function editKategori($id)
+    {
+        $kategori = Kategori::with('subKategori')->whereId($id)->whereKorpusId(Auth::user()->korpus->id)->get();
+        dd($kategori);
+        return view("pic.edi_kategori")->with("kategori", $kategori);
     }
 
 
